dda6d6e59bd1e48357c789341e93c39f6ea1a968
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2023-03-23 10:12:36 -0500

    first commit

---
+++ src/main/java/com/ggi/GgiApplication.java
@@ -0,0 +1,13 @@
+package com.ggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggi/GgiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
