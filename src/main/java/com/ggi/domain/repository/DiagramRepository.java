eac1beb72238a469438a175c0b2a8f4a4880d483
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2023-03-28 18:16:38 -0500

    fix: repositories

--- src/main/java/com/ggi/domain/repository/DiagramRepository.java
+++ src/main/java/com/ggi/domain/repository/DiagramRepository.java
@@ -3,9 +3,11 @@ package com.ggi.domain.repository;
 import com.ggi.domain.model.Client;
 import com.ggi.domain.model.Diagram;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface DiagramRepository extends JpaRepository<Diagram, Long> {
     public Optional<Diagram> findById(Long id);
 }
