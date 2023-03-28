5fc549b714df2b111bb8af57d80fd6058b8089b1
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2023-03-28 18:30:06 -0500

    feat: add componentService

---
+++ src/main/java/com/ggi/domain/service/ComponentService.java
@@ -0,0 +1,15 @@
+package com.ggi.domain.service;
+
+import com.ggi.domain.model.Component;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+public interface ComponentService {
+    Page<Component> getAll(Pageable pageable);
+    Component getById(Long id);
+    Component create(Component component);
+    Component update(Long id, Component componentRequest);
+    ResponseEntity<?> delete(Long id);
+    Component getByTag(String tag);
+}
