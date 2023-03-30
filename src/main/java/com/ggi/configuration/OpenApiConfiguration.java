d023b78a7efb692cc49b4b0916a0f7b347dad570
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2023-03-30 18:18:32 -0500

    feat: configuration added

---
+++ src/main/java/com/ggi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,19 @@
+package com.ggi.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean(name = "ggiOpenApi")
+    public OpenAPI ggiOpenApi() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("GGI Application API")
+                        .description("Blogging API implemented with Spring Boot RESTful service and documented using springdoc-openapi and OpenAPI 3.1"));
+    }
+}
