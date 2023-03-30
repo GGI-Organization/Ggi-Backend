ed25191bb5bb6490090a01c5e95d7b5b4af06985
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2023-03-30 17:52:04 -0500

    feat: interfaces done

--- src/main/java/com/ggi/domain/service/AzureConnectService.java
+++ src/main/java/com/ggi/domain/service/AzureConnectService.java
@@ -1,4 +1,14 @@
 package com.ggi.domain.service;
 
+import com.ggi.domain.model.AzureConnect;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
 public interface AzureConnectService {
+    Page<AzureConnect> getAll(Pageable pageable);
+    AzureConnect getById(Long id);
+    AzureConnect create(AzureConnect azureConnect);
+    AzureConnect update(Long id, AzureConnect azureConnect);
+    ResponseEntity<?> delete(Long id);
 }
