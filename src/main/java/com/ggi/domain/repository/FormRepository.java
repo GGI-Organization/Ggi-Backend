9ceebb0bf9da16f1445c22e766fddc3130bacfb3
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2023-04-11 17:17:36 -0500

    feat: form and formManager repositories added

---
+++ src/main/java/com/ggi/domain/repository/FormRepository.java
@@ -0,0 +1,16 @@
+package com.ggi.domain.repository;
+
+import com.ggi.domain.model.Form;
+import com.ggi.domain.model.FormManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FormRepository extends JpaRepository<Form, Long> {
+    Page<FormManager> findByFormManagerId(Long id, Pageable pageable);
+    Optional<FormManager> findByIdAndFormManagerId(Long id, Long formDiagramId);
+}
